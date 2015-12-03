ee68acb69e1a54c2ae899da906ff32bca96c5d2f
ngduyanhece <ngduyanhece@gmail.com>
2015-12-03 21:53:49 +0700

    add full_title test helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   #Returns the full title on the per-page basics.
-  def full_page(page_title = "")
+  def full_title(page_title = "")
     base_title = "Keblog"
     return base_title if page_title.empty?
     page_title + " | " + base_title
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,6 +7,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path   
+    assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")   
+  end
+  test "full title helper" do 
+  	assert_equal full_title, "Keblog"
+  	assert_equal full_title("Help"), "Help | Keblog"
   end
 end
