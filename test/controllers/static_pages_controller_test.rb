c747f0b082495841e320a27ab058a13438d7665e
ngduyanhece <ngduyanhece@gmail.com>
2015-12-03 21:31:10 +0700

    add some styles to layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Keblog"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Keblog"
+  end
+
+
 end
