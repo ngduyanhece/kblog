aaba65ae82c23941dffe8e4b4d41c14f9857978b
ngduyanhece <ngduyanhece@gmail.com>
2015-12-03 15:27:45 +0700

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  #Returns the full title on the per-page basics.
+  def full_page(page_title = "")
+    base_title = "Keblog"
+    return base_title if page_title.empty?
+    page_title + " | " + base_title
+  end
 end
